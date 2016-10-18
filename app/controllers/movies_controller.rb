a3a36118e7ad144a3d263db372470600851fa034
Miles Butler <mbutle35@uncc.edu>
2016-10-18 05:11:16 +0000

    Create the show controller and view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,3 +4,9 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 end
+
+def show
+  id = params[:id] # retrieve movie ID from URI route
+  @movie = Movie.find(id) # look up movie by unique ID
+  # will render app/views/movies/show.html.haml by default
+end
