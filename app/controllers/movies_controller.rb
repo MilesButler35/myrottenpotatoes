8a47a670fec43be7acfc5ef2372f2ae70c90792e
Miles Butler <mbutle35@uncc.edu>
2016-10-24 09:15:54 +0000

    Complete the create functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,3 +15,16 @@ def new
   @movie = Movie.new
   # default: render 'new' template
 end  
+
+def create
+ @movie = Movie.create!(user_params)
+ flash[:notice] = "#{@movie.title} was successfully created."
+ redirect_to movies_path
+end
+
+private
+def user_params
+ params.require(:movie)
+ params[:movie].permit(:title,:rating,:release_date)
+end
+
